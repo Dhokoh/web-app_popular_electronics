52b7bd791afe531b2bb3185373925e271ff6033a
David <davidhogarcia@gmail.com>
2023-07-16 22:20:25 -0500

    WebAppContainer: some elements have been added. Still incomplete, Routing is also pending

--- src/assets/components/WebAppContainer.js
+++ src/assets/components/WebAppContainer.js
@@ -1,17 +1,26 @@
 // Dependencies imports
 import { Outlet } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Other imports
 
+
 // Component constructor
-function WebAppContainer(){
-    return(   
-        <div class="flex-column">
-            <div class="container-fluid">Upper bond</div>
-            <div className='p2'> <Outlet /> </div>
-            <div class="container-fluid">Lower bond</div>
-        </div>
+function WebAppContainer() {
+    return (
+        <>
+            <nav className='nav justify-content-center sticky-top'>
+                <strong><a className='nav-link nav_link' aria-current='page' href={require('../../pages/Home')}>Inicio</a></strong>
+                <strong><a className='nav-link nav_link' aria-current='page' href={require('../../pages/Home')}>Productos</a></strong>
+                <strong><a className='nav-link nav_link' aria-current='page' href={require('../../pages/Home')}>Contacto</a></strong>
+                <form action={require('../../pages/Home')} class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"></input>
+                    <button class="btn btn-info color" type="submit">Buscar</button>                   
+                </form>
+            </nav>
+            <Outlet />
+            <nav className='nav justify-content-center fixed-bottom'>Footer Nav</nav>
+        </>
     )
 }
 
