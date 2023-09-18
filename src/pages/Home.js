bb3c693251208ea12c4caef8988445cd75a30b07
Dhokoh <davidhogarcia@gmail.com>
2023-09-18 18:46:28 -0400

    Added carousel for dev purposes in Home. Added a few markers for additional areas as well

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,34 @@ function Home(){
     return(
         <>
             <div className='container-fluid page-separator'></div>
-            <div className='container-fluid'>Hello World, homepage here</div>
-            
+            <div className='container-fluid'>
+                <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src={require('../assets/images/carousel_dev_0.png')} className="d-block w-100" alt="..."></img>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={require('../assets/images/carousel_dev_1.png')} className="d-block w-100" alt="..."></img>
+                        </div>
+                        <div class="carousel-item">
+                            <img src={require('../assets/images/carousel_dev_2.png')} className="d-block w-100" alt="..."></img>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+            <div>Purchase options and specs</div>
+            <div>Herramientas Componentes Producto terminado</div>
+            <div>Product of the day/week</div>
+            <div>Brands sold</div>
+            <div>Payment methods</div>
         </>
     )
 }
